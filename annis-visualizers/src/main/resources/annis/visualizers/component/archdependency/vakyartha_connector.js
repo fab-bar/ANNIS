16039de2d0a8eaaed489c352be23a1d14adad3b9
Thomas Krause <thomaskrause@posteo.de>
2015-08-21 18:54:56 +0200

    add some code for the "new" Arch Dependency visualizer which will not be using an iframe any longer

---
+++ annis-visualizers/src/main/resources/annis/visualizers/component/archdependency/vakyartha_connector.js
@@ -0,0 +1,10 @@
+window.annis_visualizers_component_archdependency_VakyarthaDependencyTree = function() {
+  
+  var connector = this;
+  var rootDiv = this.getElement(this.getConnectorId());
+  
+  this.onStateChange = function()
+  {
+
+  };
+};
