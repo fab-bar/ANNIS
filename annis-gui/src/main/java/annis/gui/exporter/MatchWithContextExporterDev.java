0e36fb5ceba6d4e327b42c4fb1c1e6b573d02ff0
irina <irina.glushanok@gmail.com>
2016-10-17 19:14:46 +0200

    get filter parameter

--- annis-gui/src/main/java/annis/gui/exporter/MatchWithContextExporterDev.java
+++ annis-gui/src/main/java/annis/gui/exporter/MatchWithContextExporterDev.java
@@ -68,6 +68,8 @@ public class MatchWithContextExporterDev extends SaltBasedExporter
 {
 	private static final String TRAV_IS_DOMINATED_BY_MATCH = "IsDominatedByMatch";
 	private static final String TRAV_SPEAKER_HAS_MATCHES = "SpeakerHasMatches";
+	public static final String FILTER_PARAMETER_KEYWORD = "filter";
+	public static final String FILTER_PARAMETER_SEPARATOR = ",";
 	private static HashMap <String, Boolean> speakerHasMatches = new HashMap<String, Boolean>();
 	private static String speakerName;
 	private boolean isFirstSpeakerWithMatch = true;   
@@ -78,6 +80,7 @@ public class MatchWithContextExporterDev extends SaltBasedExporter
 	private static Map <Long, List<Long>> dominanceListsWithHead = new HashMap <Long, List<Long>>();
 	private static Set<Long> inDominanceRelation = new HashSet<Long>();
   
+	private static Set<Long> filterNumbers = new HashSet<Long>();    
 	
   private static class IsDominatedByMatch implements GraphTraverseHandler
   {
@@ -106,8 +109,8 @@ public class MatchWithContextExporterDev extends SaltBasedExporter
     	 
     	 if(matchedAnno != null)
 	      {
-	        matchedNode = matchedAnno.getValue_SNUMERIC();	       
-	        
+	        matchedNode = matchedAnno.getValue_SNUMERIC();	  
+	       	        
 	        if (traversalId.equals(TRAV_SPEAKER_HAS_MATCHES))
 	        {
 	        dominatedMatchedCodes.add(matchedNode);
@@ -178,12 +181,39 @@ public class MatchWithContextExporterDev extends SaltBasedExporter
   { 
   	String currSpeakerName = "";
 	String prevSpeakerName = "";
+	 //TODO why does match number start with -1? 
+	//if match number == -1, reset global variables 
+	if (matchNumber == -1){
+		isFirstSpeakerWithMatch = true;
+	}
 	
-		
 	    
     if(graph != null)
     {
       List<SToken> orderedToken = graph.getSortedTokenByText();
+         
+      //extract filter numbers
+      if (isFirstSpeakerWithMatch && args.containsKey(FILTER_PARAMETER_KEYWORD)){
+    	 
+    	  filterNumbers.clear();
+    	  String parameters = args.get(FILTER_PARAMETER_KEYWORD);
+    	  String [] numbers = parameters.split(",");
+        	  for (int i=0; i< numbers.length; i++){
+    		  try {
+    			 Long number = Long.parseLong(numbers[i]);
+    			 filterNumbers.add(number);
+     			 
+    		  }
+    		  catch(NumberFormatException e){
+    			  //TODO
+    		  }
+    		  
+    	  }
+    	  
+      }
+      
+      
+      
       
       if(orderedToken != null)
       {
@@ -326,11 +356,7 @@ public class MatchWithContextExporterDev extends SaltBasedExporter
         
         
         
-      //TODO why does match number start with -1? 
-    	//if match number == -1, reset global variables 
-    	if (matchNumber == -1){
-    		isFirstSpeakerWithMatch = true;
-    	}
+     
     	
             
         while(it.hasNext())
